b0f9996cddfdd65494ba4a4b48fabc6b94623078
Nicolas Boely <nboely@kepler.space>
2021-07-09 13:00:16 -0400

    itur library added as a required dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='open_source_i_n',
       long_description=open('README.md').read(),
       python_requires='>=3.5',
       setup_requires=['setuptools', 'wheel'],
-      install_requires=['matplotlib', 'numpy', 'pandas', 'recordclass', 'scipy', 'skyfield'],
+      install_requires=['matplotlib', 'numpy', 'pandas', 'recordclass', 'scipy', 'skyfield', 'itur'],
       extras_require={
           'dev': [
               'pylint',
